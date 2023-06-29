eca0af0b7692f22db46c513f49bd70b9d764a493
Mark Robinson <mark.robinson@codeup.com>
2023-06-29 09:30:28 -0500

    first commit

---
+++ src/main/java/docrob/springblog/CerberusSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package docrob.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerberusSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CerberusSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/docrob/springblog/CerberusSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package docrob.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerberusSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

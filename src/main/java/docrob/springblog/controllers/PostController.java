37f83079bfac4201b761fe5b51437c1ce67fb6fe
Lola Castiilo <larrycastillo2009@gmail.com>
2023-06-30 16:39:06 -0500

    views exercise review

---
+++ src/main/java/docrob/springblog/controllers/PostController.java
@@ -0,0 +1,28 @@
+package docrob.springblog.controllers;
+
+import docrob.springblog.models.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PostController {
+    @GetMapping("/post")
+    public String post(Model model){
+        model.addAttribute("post", new Post("Hello", "Hello World"));
+        return "/posts/index";
+    }
+
+    @GetMapping("/posts")
+    public String posts(Model model){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post("Hello", "Hello World"));
+        posts.add(new Post("Hello Part 2", "Hello World Again :)"));
+        model.addAttribute("posts",posts);
+        return "/posts/show";
+    }
+
+}

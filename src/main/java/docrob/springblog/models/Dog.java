d79a9c03050e01b9c58e3f6a11e1e8077affc4fb
Mark Robinson <mark.robinson@codeup.com>
2023-07-05 12:25:52 -0500

    post jpa lec

---
+++ src/main/java/docrob/springblog/models/Dog.java
@@ -0,0 +1,28 @@
+package docrob.springblog.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "dogs")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "dog_name", length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(length = 255)
+    private String ownerName;
+
+}
